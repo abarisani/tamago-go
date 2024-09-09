d39258eff0029e2ed4135a07749a6a932cd27c3d
Andrea Barisani <andrea@inversepath.com>
2024-09-09 17:02:43 +0200

    improve GOOS=tamago test coverage

--- src/reflect/all_test.go
+++ src/reflect/all_test.go
@@ -5990,7 +5990,7 @@ func TestStructOfWithInterface(t *testing.T) {
 	rt := StructOf(fields)
 	rv := New(rt).Elem()
 
-	// tamago does not support nil pointer panics under testing
+	// tamago does not yet handle signals under testing
 	if runtime.GOOS != "tamago" {
 		// This should panic since the pointer is nil.
 		shouldPanic("", func() {
@@ -6008,7 +6008,7 @@ func TestStructOfWithInterface(t *testing.T) {
 	rt = StructOf(fields)
 	rv = New(rt).Elem()
 
-	// tamago does not support nil pointer panics under testing
+	// tamago does not yet handle signals under testing
 	if runtime.GOOS != "tamago" {
 		// This should panic since the pointer is nil.
 		shouldPanic("", func() {
