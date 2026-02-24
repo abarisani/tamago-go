09ceb1dd56e21fa58b9d9aa30df3600c6ea3a5f5
Andrea Barisani <andrea@inversepath.com>
2026-02-24 19:26:41 +0100

    migrate WakeG to os/signal

--- src/runtime/sys_tamago_arm64.s
+++ src/runtime/sys_tamago_arm64.s
@@ -47,17 +47,7 @@ TEXT runtime·rt0_arm64_tamago(SB),NOSPLIT|NOFRAME,$0
 	BL	runtime·mstart(SB)
 	UNDEF
 
-// func GetG() (gp uint, pp uint)
-TEXT runtime·GetG(SB),NOSPLIT,$0-16
-	MOVD	g, gp+0(FP)
-
-	MOVD	(g_m)(g), R0
-	MOVD	(m_p)(R0), R0
-	MOVD	R0, pp+8(FP)
-
-	RET
-
-// WakeG modifies a goroutine cached timer for time.Sleep (g.timer) to fire as
+// wakeg modifies a goroutine cached timer for time.Sleep (g.timer) to fire as
 // soon as possible.
 //
 // The function arguments must be passed through the following registers
@@ -69,7 +59,7 @@ TEXT runtime·GetG(SB),NOSPLIT,$0-16
 // (rather than on the frame pointer):
 //
 //   * R0: success (0), failure (1)
-TEXT runtime·WakeG(SB),NOSPLIT|NOFRAME,$0-0
+TEXT runtime·wakeg(SB),NOSPLIT|NOFRAME,$0-0
 	CMP	$0, R0
 	BEQ	fail
 
@@ -131,14 +121,6 @@ check:
 	RET
 fail:
 	MOVD	$1, R0
-	RET
-
-// func Wake(gp uint) bool
-TEXT runtime·Wake(SB),$0-9
-	MOVD	gp+0(FP), R0
-	CALL	runtime·WakeG(SB)
-	EOR	$1, R0
-	MOVB	R0, ret+8(FP)
 	RET
 
 // never called (cgo not supported)
