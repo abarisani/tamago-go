65b49aefa5f6275dab20944025e6b55812d074ce
Andrea Barisani <andrea@inversepath.com>
2026-02-25 15:29:20 +0100

    align GOOS=tamago API

--- src/os/signal/signal_tamago_arm64.s
+++ src/os/signal/signal_tamago_arm64.s
@@ -13,7 +13,7 @@ TEXT ·Relay(SB),NOSPLIT|NOFRAME,$0-8
 	RET
 
 // func Waiting() bool
-TEXT ·Waiting(SB),NOSPLIT|NOFRAME,$0-1
+TEXT ·Waiting(SB),NOSPLIT,$0-1
 	MOVD	·loopG(SB), R0
 	CALL	runtime·findTimer(SB)
 	EOR	$1, R1
