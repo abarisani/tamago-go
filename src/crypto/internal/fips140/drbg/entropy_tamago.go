81c2b50a5bb48572ede2d8bcff946a3a4276f65f
Andrea Barisani <andrea@inversepath.com>
2026-05-09 15:10:41 +0200

    align GOSOFT=1 implementation to tamago1.26.3

--- src/crypto/internal/fips140/drbg/entropy_tamago.go
+++ src/crypto/internal/fips140/drbg/entropy_tamago.go
@@ -8,7 +8,9 @@ package drbg
 
 import (
 	entropy "crypto/internal/entropy/v1.0.0"
+	"crypto/internal/sysrand"
 	"sync"
+	"sync/atomic"
 )
 
 // memory is a scratch buffer that is accessed between samples by the entropy
